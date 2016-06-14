e2c08422bb6682769a24b7b94a121f79c7e4dcf9
U-OFG\Matej.Ferenc <Matej.Ferenc@CZIT-NTB-024.ofg.local>
2016-06-14 16:18:00 +0200

    asdfasdf

--- bimbo/script.py
+++ bimbo/script.py
@@ -18,7 +18,13 @@ def rmsle(p, a):
     n = len(p)
     sum = 0
     for i in range(0, n - 1):
-        sum += (math.log(p[i] + 1) - math.log(a[i] + 1)) ** 2
+        try:
+            sum += (math.log(p[i] + 1) - math.log(a[i] + 1)) ** 2
+        except ValueError:
+            print p[i]
+            print a[i]
+            print (math.log(p[i] + 1) - math.log(a[i] + 1))
+            print (math.log(p[i] + 1) - math.log(a[i] + 1)) ** 2
     return math.sqrt(sum / n)
 
 def run_solution():
@@ -28,9 +34,13 @@ def run_solution():
     f.readline()
     hits = defaultdict(int)
     total = 0
+    estimated = 0
+    notEstimated = 0
     
     p = []
     a = []
+    
+    client_product_count = defaultdict(lambda: defaultdict(int))
 
     # Calc counts
     while 1:
@@ -47,15 +57,17 @@ def run_solution():
             canal_id = arr[2]
             ruta_sak = arr[3]
             cliente_id = int(arr[4])
-            producto_id = arr[5]
+            producto_id = int(arr[5])
             venta_uni_hoy = int(arr[6])
             venta_hoy = arr[7]
             dev_uni_proxima = arr[8]
             dev_proxima = arr[9]
             demanda_uni_equil = int(arr[10])
         
-        if validate == 1 and cliente_id % N0 == N1:
+        if validate == 1 and total % N0 == N1:
             continue
+        
+        client_product_count[(cliente_id, producto_id)][semana] += demanda_uni_equil
 
     f.close()
     ###########################
@@ -73,7 +85,6 @@ def run_solution():
     out = open(path, "w")
     f.readline()
     total = 0
-    totalv = 0
     out.write("id,Demanda_uni_equil\n")
 
     while 1:
@@ -92,16 +103,18 @@ def run_solution():
             canal_id = arr[2]
             ruta_sak = arr[3]
             cliente_id = int(arr[4])
-            producto_id = arr[5]
+            producto_id = int(arr[5])
             venta_uni_hoy = int(arr[6])
             venta_hoy = arr[7]
             dev_uni_proxima = int(arr[8])
             dev_proxima = arr[9]
             demanda_uni_equil = int(arr[10])
-            if cliente_id % N0 != N1:
+            if total % N0 != N1:
                 continue
             a.append(demanda_uni_equil)
-            p.append(venta_uni_hoy - dev_uni_proxima)
+#             p.append(math.fabs(venta_uni_hoy - dev_uni_proxima))
+#             if venta_uni_hoy - dev_uni_proxima < 0:
+#                 print "venta_uni_hoy - dev_uni_proxima < 0"
         else:
             id = int(arr[0])
             semana = int(arr[1])
@@ -111,6 +124,18 @@ def run_solution():
             cliente_id = arr[5]
             producto_id = arr[6]
 
+            
+        if (cliente_id, producto_id) in client_product_count:
+            counts = client_product_count[(cliente_id, producto_id)]
+            n = len(counts)
+            sum = 0
+            for i in range(0, n - 1):
+                sum += counts[i]
+            p.append(sum / n)
+            estimated += 1
+        else:
+            p.append(0)
+            notEstimated += 1
 
         
 #         out.write(str(id) + ',')
@@ -127,6 +152,8 @@ def run_solution():
         print(" validation")
         print("----------------------------------------------------------------")
         print("RMSLE = %8.4f " % (rmsle(p, a)))
+        print("estimated: %f" % (estimated))
+        print("not estimated: %f" % (notEstimated))
     # <<< validation
 
 run_solution()
